538ba5b8b04a373ad73f29f61466d2e60aeab6f2
746ejjd83 <omed.pubg12@gmail.com>
2024-08-21 00:00:10 +0300

    Update ddifj.lua

--- ddifj.lua
+++ ddifj.lua
@@ -1,1 +1,17 @@
-"Free Version"
+{
+    "MRNOOBYLOL3",
+    "TPS_GL0RY",
+    "Dahoodpro237",
+    "ov5or5",
+    "KURDKINGNN",
+    "bloxfruitplayerpr0",
+    "ov5or6",
+    "Dahoodreseller0",
+    "dahoodcash65",
+    "Dahood4689",
+    "sehldan",
+    "Dahoodcash700",
+    "Dahoodtrader8",
+    "Dahoodseller_2"
+
+}
